46f49f21e7207eb90e83bc5e5aa469161ee7d098
S.Gokay <63980772+Gokay2705@users.noreply.github.com>
2020-11-12 21:17:16 +0100

    Update hello-word.py
    updated

--- hello-word.py
+++ hello-word.py
@@ -1,3 +1,4 @@
 print('Hello World')
 print("This is my first jenkins python webhook application")
 print("Jenkins make push command in github instead of me.")
+print("This is may 2.nd turn of projecet")
